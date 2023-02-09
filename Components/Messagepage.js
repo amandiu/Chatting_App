c1f578cf7e0c0eaad4ca293a2e74b7106ae025f1
Md.Amanullah Akon <79790568+amandiu@users.noreply.github.com>
2023-02-09 19:49:39 +0600

    Chatting App

---
+++ Components/Messagepage.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+
+export default function Messagepage() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages) => {
+    //console.log(messages)
+    setMessages(previousMessages => GiftedChat.append(previousMessages,   {
+        _id: messages[0]._id,
+        text: messages[0].text,
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },))
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
+}
